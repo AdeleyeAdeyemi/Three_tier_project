9621dbef71e14d41d512f141812038459948aa7f
Adeleye_Adeyemi <josekadel355@gmail.com>
2026-09-16 11:59:03 +0100

    Fix syntax errors in authentication.ts

--- src/application/authentication/authentication.ts
+++ src/application/authentication/authentication.ts
@@ -2,15 +2,16 @@ let req;
 
 // The data structure that will save the updates from the web shell
 req = {
-   updateMessage: { timestamp, title },
-   errors: Array,
+    updateMessage: { timestamp, title },
+    errors: Array,
     messages: Array,
 }
 
 export function myPlatformContainer() {
     return new HTMLContainer(
         "myPlatform",
-        className: "container mb-4"
+        {
+            className: "container mb-4"
+        }
     )
-
 }
