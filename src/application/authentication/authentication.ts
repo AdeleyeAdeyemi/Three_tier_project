de66f52ae952919824ba95b705e4cb76478ebc09
AdeleyeAdeyemi <josekadel355@gmail.com>
2026-09-16 11:39:49 +0100

    Initial commit

---
+++ src/application/authentication/authentication.ts
@@ -0,0 +1,16 @@
+let req;
+
+// The data structure that will save the updates from the web shell
+req = {
+   updateMessage: { timestamp, title },
+   errors: Array,
+    messages: Array,
+}
+
+export function my-platform-container() {
+    return new HTMLContainer(
+        "my-platform",
+        class="container mb-4"
+    )
+
+}
